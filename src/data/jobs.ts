cc3aebafa9a6a85c5d75c456282b6dfdc53c6e00
Iarlen Aquiles <iarlensilva775@gmail.com>
2023-08-03 15:03:56 -0300

    feat(): added data

---
+++ src/data/jobs.ts
@@ -0,0 +1,39 @@
+export const jobs = [
+    {
+        "ID": 1,
+        "Descricao": "Importação de arquivos de fundos",
+        "Data Máxima de conclusão": '2021-02-04 12:00:00',
+        "Tempo estimado": '8 horas'
+    },
+    {
+        "ID": 2,
+        "Descricao": 'Importação de dados da Base Legada',
+        "Data Máxima de conclusão": '2021-02-04 12:00:00',
+        "Tempo estimado": '4 horas'
+    },
+    {
+        "ID": 3,
+        "Descricao": 'Importação de dados',
+        "Data Máxima de conclusão": '2021-02-02 12:00:00',
+        "Tempo estimado": '6 horas'
+    },
+    {
+        "ID": 4,
+        "Descricao": 'Desenvolver historia 745',
+        "Data Máxima de conclusão": '2021-02-02 12:00:00',
+        "Tempo estimado": '2 horas'
+    },
+    {
+        "ID": 5,
+        "Descricao": 'Gerar QRCode',
+        "Data Máxima de conclusão": '2020-02-15 12:00:00',
+        "Tempo estimado": '6 horas'
+    },
+    {
+        "ID": 6,
+        "Descricao": 'Importação de dados de integração',
+        "Data Máxima de conclusão": '2020-02-15 12:00:00',
+        "Tempo estimado": '8 horas'
+    },
+]
+
